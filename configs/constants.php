ddd5eb4d69e0ac26fb8ce69ee42f57ccf240058f
MishaB28 <mishabhargava28@gmail.com>
2026-03-09 00:15:37 +0530

    Session fix

--- configs/constants.php
+++ configs/constants.php
@@ -1,9 +1,6 @@
 <?php
 ob_start();
 // start session
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
 
 $scheme = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
 $host   = $_SERVER['HTTP_HOST'] ?? 'localhost';
--- partials-front/menu.php
+++ partials-front/menu.php
@@ -1,4 +1,8 @@
 <?php
+ob_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 require_once __DIR__ . '/../configs/constants.php';
 require_once __DIR__ . '/../connection.php';
 ?>
