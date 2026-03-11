7c0953f3fd6381da92a3ff53a5d77ab9a993627a
MishaB28 <mishabhargava28@gmail.com>
2026-03-11 21:57:08 +0530

    Mail changes for Brevo

--- pages/contactmail.php
+++ pages/contactmail.php
@@ -1,61 +1,62 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-require_once __DIR__ . '/../phpmailer/Exception.php';
-require_once __DIR__ . '/../phpmailer/PHPMailer.php';
-require_once __DIR__ . '/../phpmailer/SMTP.php';
-
-$mail = new PHPMailer(true);
 $alert = '';
+
 if (isset($_POST['submit'])) {
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $subject = $_POST['subject'];
-  $message = $_POST['message'];
-  try {
-    $mail->isSMTP();
-    $mail->Host = 'smtp-relay.brevo.com';
-    $mail->SMTPAuth = true;
-    $mail->Timeout    = 10;
-    // Gmail address which you want to use as SMTP server
-    $mail->Username = $_SERVER['SERVER_NAME'] === 'localhost'
-      ? 'MAIL_ID'
-      : getenv('MAIL_ID');
-    // Gmail address App Password
-    $mail->Password = $_SERVER['SERVER_NAME'] === 'localhost'
-      ? 'MAIL_APP_PASSWORD'
-      : getenv('MAIL_PASSWORD');
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = '587';
-    // Gmail address which you used as SMTP server
-    $mail->setFrom(
-        $_SERVER['SERVER_NAME'] === 'localhost'
-            ? 'MAIL_ID'
-            : getenv('MAIL_ID')
-    );
-    // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
-    $mail->addAddress(
-        $_SERVER['SERVER_NAME'] === 'localhost'
-            ? 'MAIL_ID'
-            : getenv('MAIL_RECEIVER')
-    );
-    $mail->isHTML(true);
-    $mail->Subject = "Message Received (Contact Page), Issue : $subject";
-    $mail->Body = "<h3>Name : $name <br>Email: $email <br>Message : $message</h3>";
-    $mail->send();
-    $alert = '<div class="alert alertsuccess">
-                 <span>Your Message has been sent! Thank you for contacting us. We will get back to you as soon as we can! :)</span>
-                </div>';
-  } catch (Exception $e) {
-        error_log("Mailer Error: " . $mail->ErrorInfo);
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $apiKey = getenv('MAIL_API');
+    $fromEmail = getenv('MAIL_ID');
+    $toEmail = getenv('MAIL_ID'); // where messages should go
+    $fromName = 'The Craft Nest';
+
+    $body = [
+        "sender" => ["name" => $fromName, "email" => $fromEmail],
+        "to" => [["email" => $toEmail]],
+        "subject" => "Message Received (Contact Page), Issue: $subject",
+        "htmlContent" => "<h3>Name : $name <br>Email: $email <br>Message : $message</h3>"
+    ];
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://api.brevo.com/v3/smtp/email");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        "accept: application/json",
+        "api-key: $apiKey",
+        "content-type: application/json"
+    ]);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($body));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+    $err = curl_error($ch);
+    curl_close($ch);
+
+    if ($err) {
+        error_log("Mailer Error: " . $err);
         $alert = '<div class="alert alerterror">
                     <span>We are so sorry :(  There was a problem. Please try again later!</span>
                   </div>';
-  }
+    } else {
+        $res = json_decode($response, true);
+        if (isset($res['messageId'])) {
+            $alert = '<div class="alert alertsuccess">
+                         <span>Your Message has been sent! Thank you for contacting us. We will get back to you as soon as we can! :)</span>
+                      </div>';
+        } else {
+            error_log("Mailer Error: " . $response);
+            $alert = '<div class="alert alerterror">
+                        <span>We are so sorry :(  There was a problem. Please try again later!</span>
+                      </div>';
+        }
+    }
 }
 ?>
 <script>
    setTimeout(function() {
       let alert = document.querySelector(".alert");
-      if (alert) alert.remove();                              // ADDED null check
+      if (alert) alert.remove();
    }, 8000);
 </script>
--- pages/forgotpassword.php
+++ pages/forgotpassword.php
@@ -1,58 +1,56 @@
 <?php
 require_once __DIR__ . '/../connection.php';
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
 $alert = '';
+
 function sendMail($emailid, $reset_token)
 {
-    require_once __DIR__ . '/../phpmailer/PHPMailer.php';
-    require_once __DIR__ . '/../phpmailer/SMTP.php';
-    require_once __DIR__ . '/../phpmailer/Exception.php';
+    $apiKey = getenv('MAIL_API');
+    $fromEmail = getenv('MAIL_ID');
+    $fromName = 'The Craft Nest';
 
-    $mail = new PHPMailer(true);
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https://" : "http://";
+    $host = $_SERVER['HTTP_HOST'];
+    $resetLink = $protocol . $host . BASE_URL . "pages/updatepassword.php?emailid="
+                 . urlencode($emailid) . "&reset_token=" . urlencode($reset_token);
 
-    try {
-        //Server settings
-        $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp-relay.brevo.com';                     //Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                 //Enable SMTP authentication
-        $mail->Timeout    = 10;
-        $mail->Username   = $_SERVER['SERVER_NAME'] === 'localhost'
-                              ? 'MAIL_ID'
-                              : getenv('MAIL_USER'); //SMTP username
-        $mail->Password   = $_SERVER['SERVER_NAME'] === 'localhost'
-                              ? 'MAIL_APP_PASSWORD'
-                              : getenv('MAIL_PASSWORD'); //SMTP password
-        $fromEmail = $_SERVER['SERVER_NAME'] === 'localhost'
-                                 ? 'MAIL_ID'
-                                 : getenv('MAIL_ID');
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;           //Enable implicit TLS encryption
-        $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $body = [
+        "sender" => ["name" => $fromName, "email" => $fromEmail],
+        "to" => [["email" => $emailid]],
+        "subject" => "Password reset link - The Craft Nest",
+        "htmlContent" => "
+            We got a password reset request from you.<br>
+            Please click on the link below to reset your password!<br><br>
+            <a href='$resetLink'>Reset your password</a>
+        "
+    ];
 
-        //Recipients
-        $mail->setFrom($fromEmail, 'The Craft Nest');
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://api.brevo.com/v3/smtp/email");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        "accept: application/json",
+        "api-key: $apiKey",
+        "content-type: application/json"
+    ]);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($body));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-        $mail->addAddress($emailid);     //Add a recipient
+    $response = curl_exec($ch);
+    $err = curl_error($ch);
+    curl_close($ch);
 
-        //Content
-        $mail->isHTML(true); //Set email format to HTML
-        $mail->Subject = 'Password reset link - The Craft Nest';
-        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https://" : "http://";
-        $host = $_SERVER['HTTP_HOST'];
-        $resetLink = $protocol . $host . BASE_URL . "pages/updatepassword.php?emailid="
-            . urlencode($emailid) . "&reset_token=" . urlencode($reset_token);
-        $mail->Body = "We got a password reset request from you.<br>
-        Please click on the link below to reset your password!<br><br>
-        <a href='$resetLink'>Reset your password</a>";
+    if ($err) {
+        error_log("Mailer Error: " . $err);
+        return false;
+    }
 
-        $mail->send();
+    $res = json_decode($response, true);
+    if (isset($res['messageId'])) {
         return true;
-    } catch (Exception $e) {
-          error_log("Mailer Error: " . $mail->ErrorInfo);
-          return false;
-      }
+    } else {
+        error_log("Mailer Error: " . $response);
+        return false;
+    }
 }
 
 if (isset($_POST['send-reset-link'])) {
@@ -85,8 +83,8 @@ if (isset($_POST['send-reset-link'])) {
     </div>';
     }
 }
-
 ?>
+
 <script>
    setTimeout(function() {
        let alert = document.querySelector(".alert");
