46b2bb086d0e0eff93b67a61e75504e129baa2f3
MishaB28 <mishabhargava28@gmail.com>
2026-03-11 23:52:40 +0530

    Login fix

--- admin/partials/menu.php
+++ admin/partials/menu.php
@@ -1,33 +1,33 @@
-<?php include('../configs/constants.php');
+<?php
+include('../configs/constants.php');
 session_start();
-if (empty($_SESSION['login'])){
 
-
-      echo '<script> alert("Not logged in!");
-          window.location.replace("' . BASE_URL . 'admin/login.php");
-  </script>';
-  die();
-
-  }
+if (empty($_SESSION['login'])) {
+    echo '<script>alert("Not logged in!");
+          window.location.replace("' . BASE_URL . 'admin/login.php");</script>';
+    exit();
+}
 ?>
+
 <html>
-   <head>
-       <title>The Craft Nest - Home Page</title>
-       <link rel="stylesheet" href="css2/admin.css">
-   </head>
-</html>
-<body>  
+<head>
+    <title>The Craft Nest - Home Page</title>
+    <link rel="stylesheet" href="css2/admin.css">
+</head>
+<body>
     <!--Menu Section starts-->
     <div class="menu text-center">
-<div class="wrapper">
-  <ul>
-    <li><a href="index.php">Home</a></li>
-    <li><a href="manage-admin.php">Admin</a></li>
-    <li><a href="manage-category.php">Categories</a></li>
-    <li><a href="manage-product.php">Products</a></li>
-    <li><a href="manage-orders.php">Orders</a></li>
-    <li><a href="logout.php">Log out</a></li>
-  </ul>
-</div>
+        <div class="wrapper">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="manage-admin.php">Admin</a></li>
+                <li><a href="manage-category.php">Categories</a></li>
+                <li><a href="manage-product.php">Products</a></li>
+                <li><a href="manage-orders.php">Orders</a></li>
+                <li><a href="logout.php">Log out</a></li>
+            </ul>
+        </div>
     </div>
     <!--Menu Section ends-->
+</body>
+</html>
