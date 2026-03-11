415ccbce03c6c790dd2b62bca91d0a1817a56dcf
MishaB28 <mishabhargava28@gmail.com>
2026-03-11 22:28:26 +0530

    Reset token fix

--- admin/updatepasswordadmin.php
+++ admin/updatepasswordadmin.php
@@ -1,6 +1,7 @@
-<?php include('../configs/constants.php');  ?>
+<?php include('../configs/constants.php'); ?>
 <?php
 $alert = '';
+
 if (isset($_POST['updatepassword'])) {
     $pass = md5($_POST['password']);
 
@@ -8,13 +9,12 @@ if (isset($_POST['updatepassword'])) {
         $email = $_POST['emailid'];
         $token = $_POST['reset_token'];
 
-        // Make sure token is valid before updating
         $check = "SELECT * FROM `admin_users` WHERE `email`='$email' AND `resettoken`='$token'";
         $res = mysqli_query($conn, $check);
         if ($res && mysqli_num_rows($res) == 1) {
-          $update = "UPDATE `admin_users`
-                     SET `password`='$pass', `resettoken`='', `resettokenexpire`=''
-                     WHERE `email`='$email' AND `resettoken`='$token'";
+            $update = "UPDATE `admin_users`
+                       SET `password`='$pass', `resettoken`='', `resettokenexpire`=''
+                       WHERE `email`='$email' AND `resettoken`='$token'";
             if (mysqli_query($conn, $update)) {
                 echo '<script> alert("Your password has been changed! Thank you :)");
                      window.location.href = "' . BASE_URL . 'admin/login.php";
@@ -36,7 +36,21 @@ if (isset($_POST['updatepassword'])) {
        </div>';
     }
 }
+?>
+<html>
+<head>
+    <title>Login - The Craft Nest</title>
+    <link rel="stylesheet" href="css2/admin.css">
+</head>
+<body>
 
+<div class="login">
+    <h1 class="text-center">Create a new password</h1>
+    <br><br>
+    <div class="form">
+        <div class="form-box forgotpassword">
+
+<?php
 if (isset($_GET['emailid']) && isset($_GET['reset_token'])) {
     $email = $_GET['emailid'];
     $token = $_GET['reset_token'];
@@ -44,23 +58,17 @@ if (isset($_GET['emailid']) && isset($_GET['reset_token'])) {
     $query = "SELECT * FROM `admin_users` WHERE `email`='$email' AND `resettoken`='$token'";
     $result = mysqli_query($conn, $query);
     if ($result && mysqli_num_rows($result) == 1) {
-        echo "
-        <div class='login'>
-        <h1 class='text-center'>Create a new password</h1>
-        <br><br>
-        <div class='form'>
-        <div class='form-box forgotpassword'>
-            <form action='' method='POST' class='text-center'>
-                <div class='field'>
-                    <i class='uil uil-lock-alt'></i>
-                    <input type='password' placeholder='New Password' required name='password'>
-                </div>
-                <input class='btn-primary' type='submit' value='Change your password!' name='updatepassword'>
-                <input type='hidden' name='emailid' value='$email'>
-                <input type='hidden' name='reset_token' value='$token'>
-            </form>
-        </div>
-        </div>";
+        ?>
+        <form action="" method="POST" class="text-center">
+            <div class="field">
+                <i class="uil uil-lock-alt"></i>
+                <input type="password" placeholder="New Password" required name="password">
+            </div>
+            <input class="btn-primary" type="submit" value="Change your password!" name="updatepassword">
+            <input type="hidden" name="emailid" value="<?= htmlspecialchars($email) ?>">
+            <input type="hidden" name="reset_token" value="<?= htmlspecialchars($token) ?>">
+        </form>
+        <?php
     } else {
         $alert = '<div class="alert error">
         <span>Invalid or expired link.</span>
@@ -69,9 +77,20 @@ if (isset($_GET['emailid']) && isset($_GET['reset_token'])) {
 }
 echo $alert;
 ?>
+
+        </div>
+    </div>
+</div>
+
 <script>
     setTimeout(function() {
         let alert = document.querySelector(".alert");
         if(alert) alert.remove();
     }, 4000);
+
+    if (window.history.replaceState) {
+        window.history.replaceState(null, null, window.location.href);
+    }
 </script>
+</body>
+</html>
